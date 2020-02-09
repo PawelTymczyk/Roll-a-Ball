8c08abf565de9dc06978c9d0fb98a2e1f06b716f
Petrix23 <32833558+Petrix23@users.noreply.github.com>
2020-02-09 01:04:40 +0100

    Inital and Finish Project

---
+++ Roll a Ball/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHoryzont = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHoryzont, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+
+}
